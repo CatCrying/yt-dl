316696c31d5e82b005a8d7155f73644d25060cb8
CatCrying <88854387+CatCrying@users.noreply.github.com>
2025-11-28 12:35:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    // Increase limit to 1000KB (1MB) to silence the warning
+    chunkSizeWarningLimit: 1000, 
     rollupOptions: {
       output: {
         manualChunks(id) {
